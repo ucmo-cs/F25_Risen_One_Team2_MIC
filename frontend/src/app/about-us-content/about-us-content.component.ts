6617c623489cc99382ae9fe9d5fcf9c7683b891e
Kobe Johnson <kdj95550@ucmo.edu>
2025-02-26 22:29:23 -0600

    Improved darkness of hoverable cards in 'about-us' component. Added hyperlinks to 'Contact Us' card in 'about-us' component. Also began formatting the HTML and CSS of 'about-us-content' component to allow for a header and body for each individual hoverable element.

--- frontend/src/app/about-us-content/about-us-content.component.ts
+++ frontend/src/app/about-us-content/about-us-content.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { NgSwitch, CommonModule, NgSwitchCase, NgFor, NgIf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about-us-content',
   standalone: true,
+  imports: [MatCardModule, NgSwitch, NgSwitchCase, NgFor, NgIf],
   templateUrl: './about-us-content.component.html',
   styleUrls: ['./about-us-content.component.css']
 })
 export class AboutUsContentComponent {
-  @Input() message: string = '';
+  @Input() cardData: any;
 }
---
+++ frontend/src/app/constants/icon.constants.ts
@@ -0,0 +1,9 @@
+export const ICONS = {
+    //global identifiers for icons since I couldn't get the damn icons to display with relative path
+    facebook: '../assets/icons/facebook.svg',
+    linkedin: '../assets/icons/linkedin.svg',
+    instagram: '../assets/icons/instagram.svg',
+    cloudcomputing: '../assets/icons/cloudcomputing.svg',
+    cicd: '../assets/icons/cicd.svg',
+    datacentermigration: "../assets/icons/datacentermigration.svg"
+  };
