5b3325a2ed5502f49723ecc0859892970bf1639b
Zachary Pedigo <41409007+Hexman768@users.noreply.github.com>
2025-03-13 13:09:05 -0500

    SE3-23: Optimized calls to Amazon S3 Buckets (#16)

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from './user.model';
+import { UserApiService } from '../services/user.service';
 
 @Component({
   selector: 'user-profile',
@@ -10,8 +11,6 @@ import { User } from './user.model';
 
 export class UserComponent implements OnInit {
     userId: string | null = "";
-    message: string = 'Loading...';
-    users: Array<User> = [];
     user: User = {
         id: '',
         firstname: '',
@@ -27,7 +26,7 @@ export class UserComponent implements OnInit {
         team: ''
     };
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute, private userService: UserApiService) { }
 
     ngOnInit(): void {
         // Initialize component
@@ -35,24 +34,6 @@ export class UserComponent implements OnInit {
             this.userId = params.get('id');
         });
 
-        const zachBucket = 'http://pedigoprojectbucketnew.s3-website.us-east-2.amazonaws.com/lambda-output.json';
-        const calebBucket = 'http://riseonebiopagebucket.s3-website.us-east-2.amazonaws.com/lambda-output.json';
-        fetch(zachBucket)
-            .then(response => {
-                if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-                    return response.json();
-            })
-            .then(data => {
-                this.message = 'Risen One Employees';
-                this.users = data.users;
-                this.user = this.users.find(person => person.id === this.userId) || this.user;
-                console.log('Fetched users:', this.users); // Debugging log
-                console.log('Found user: ', this.user.id);
-            })
-            .catch(error => {
-                console.error('Error fetching JSON:', error);
-                this.message = `Error loading data: ${error.message}`;
-            }
-        );
+        this.user = this.userService.getUserInfo(this.userId) || this.user;
     }
 }
